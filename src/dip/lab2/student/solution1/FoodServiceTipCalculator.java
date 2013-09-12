66f387cba7ca394fbad473553e418187dab5b40b
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-12 18:00:47 -0500

    added methods and data to foodservice that is specific to foodservice

--- src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -9,10 +9,56 @@ package dip.lab2.student.solution1;
  * @author F03 H4MM3R
  */
 public class FoodServiceTipCalculator implements TipCalculator{
+    
+    private static final double MIN_BILL = 0.00;
+    private static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
+    
+    private double bill;
+    public enum ServiceQuality {
+        GOOD, FAIR, POOR
+    }
+    private ServiceQuality serviceQuality;
 
+    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
+        this.setServiceRating(q);
+        this.setBill(billAmt);
+    }
+    
     @Override
     public double getTip() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = bill * GOOD_RATE;
+                break;
+            case FAIR:
+                tip = bill * FAIR_RATE;
+                break;
+            case POOR:
+                tip = bill * POOR_RATE;
+                break;
+        }
+
+        return tip;
+    }
+    
+    public final void setBill(double billAmt) {
+        if(billAmt < MIN_BILL) {
+            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        }
+        bill = billAmt;
+    }
+
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
     }
     
 }
