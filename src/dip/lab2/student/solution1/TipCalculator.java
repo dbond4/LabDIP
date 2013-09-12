7a5c9f203086ed574e062e2e50d8633bb58bc3ca
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-12 17:09:27 -0500

    added classes and variables to TipCalculator

---
+++ src/dip/lab2/student/solution1/TipCalculator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public class TipCalculator {
+    
+}
