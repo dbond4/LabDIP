bda420a766b39488280682398027a336ac0a9e0a
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-16 16:53:31 -0500

    started a new lab2 and tried a different approach. still got stuck at pullig out services and making high lvl classes

---
+++ src/dip/lab2/studentProject/ITip.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.studentProject;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public interface ITip {
+    
+    /**
+     *
+     * @return
+     */
+    public abstract double getTip();
+    
+}
